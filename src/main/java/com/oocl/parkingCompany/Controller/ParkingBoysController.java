5241e63520bb048abf288941f531fa2404dc6fdc
GUOAB <AbbyGuo@oocl.com>
2018-07-25 14:55:25 +0800

    add addParkingBoy func

--- src/main/java/com/oocl/parkingCompany/Controller/ParkingBoysController.java
+++ src/main/java/com/oocl/parkingCompany/Controller/ParkingBoysController.java
@@ -1,9 +1,24 @@
 package com.oocl.parkingCompany.Controller;
 
-import org.springframework.stereotype.Controller;
+import com.oocl.parkingCompany.Model.ParkingBoy;
+import com.oocl.parkingCompany.Service.ParkingBoysService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/parkingBoys")
 public class ParkingBoysController {
+    @Autowired
+    private ParkingBoysService parkingBoysService;
+
+    @PostMapping
+    public List<ParkingBoy> addparkingBoy(@RequestBody ParkingBoy parkingBoy){
+        List<ParkingBoy> parkingBoyList = parkingBoysService.addparkingBoy(parkingBoy);
+        return parkingBoyList;
+    }
 }
